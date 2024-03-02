202045f28b41564aa2b712b4cd1668787432c232
Kah Boon <changkahboon25@gmail.com>
2024-03-02 19:15:02 +0800

    feat: login status

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use eframe::egui;
+use login_page::LoginPage;
 
 mod login_page;
 
@@ -17,13 +18,25 @@ fn main() -> Result<(), eframe::Error> {
             // This gives us image support:
             // egui_extras::install_image_loaders(&cc.egui_ctx);
 
-            Box::new(MyApp {})
+            Box::new(MyApp {
+                login_status: false,
+                login_page: login_page::LoginPage::default(),
+            })
         }),
     )
 }
 
-struct MyApp {}
+struct MyApp {
+    login_status: bool,
+    login_page: login_page::LoginPage,
+}
 
 impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {}
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        if self.login_status == false {
+            self.login_status = self.login_page.show(ctx);
+        } else {
+            // self.login_page
+        }
+    }
 }
