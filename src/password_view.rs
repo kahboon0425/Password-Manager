afdd302215c811eb56e9014f472f259fe3c8c6ff
Nixon <voxell.technologies@gmail.com>
2024-03-10 12:35:40 +0800

    feat: add password name for viewing

--- src/password_view.rs
+++ src/password_view.rs
@@ -1,1 +1,11 @@
+use magic_crypt::MagicCrypt256;
 
+use crate::add_password_form::PasswordData;
+
+pub fn show(ctx: &egui::Context, mc: &MagicCrypt256, password_data: &[PasswordData]) {
+    egui::CentralPanel::default().show(ctx, |ui| {
+        for pass in password_data {
+            ui.label(&pass.name);
+        }
+    });
+}
